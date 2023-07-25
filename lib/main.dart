723765e1f93bbc6a74584500dfef7d3797854d0d
Amal pk <amalashok7902@gmail.com>
2023-07-25 15:35:40 +0530

    Connecting to Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,26 @@
+import 'package:chat_with_friends/Constant/const.dart';
+import 'package:chat_with_friends/View/home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: FirebaseOptions(
+        apiKey: Constants.apiKey,
+        authDomain: Constants.authDomain,
+        projectId: Constants.projectId,
+        storageBucket: Constants.storageBucket,
+        messagingSenderId: Constants.messagingSenderId,
+        appId: Constants.appId,
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
@@ -12,11 +32,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-       
         primarySwatch: Colors.blue,
       ),
-      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomePage(),
     );
   }
 }
-
